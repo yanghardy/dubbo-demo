d22f8ed594af15ce1a7fc8fa93f646b63dd99776
UID <UID@DESKTOP-DACJOCN>
2019-06-28 19:12:15 +0800

    dubbo demo

---
+++ dubbo-jt-demo-consumer/src/main/java/com/jt/dubbo/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.jt.dubbo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jt.dubbo.pojo.User;
+import com.jt.dubbo.service.UserService;
+
+@RestController
+public class UserController {
+	//timeout=3000，超时连接3秒
+	//check=false不检查可用的服务；默认是true，如果没有可用的服务就会报错
+	@Reference(timeout=3000,check=false)
+	/**
+	 * 负载均衡策略：
+	 * loadbalance="random" 随机（默认）
+	 * loadbalance="roundRobin" 轮询（基于权重是一样的基础上）
+	 * loadbalance="leastActive" 挑选负载最少服务器
+	 * loadbalance="consistentHash" 绑定服务器ip地址
+	 * 
+	 */
+	private UserService userService;
+	
+	@RequestMapping("/findAll")
+	public List<User> findAll(){
+		
+		return userService.findAll();
+	}
+	
+	@RequestMapping("/saveUser/{name}/{age}/{sex}")//restFul动态接收参数
+	public String saveUser(User user) {
+		
+		userService.saveUser(user);
+		return "用户入库成功!!!";
+	}
+	
+	@RequestMapping("/deleteUserById")
+	public String deleteUserById(Integer...id) {
+		
+		userService.deleteUserById(id);
+		return "用户删除成功!!!";
+	}
+}
---
+++ dubbo-jt/dubbo-jt-demo-provider2/src/main/java/com/jt/dubbo/service/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.jt.dubbo.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.jt.dubbo.mapper.UserMapper;
+import com.jt.dubbo.pojo.User;
+@Service(timeout=3000)	//3秒超时	
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserMapper userMapper;
+	@Override
+	public List<User> findAll() {
+		System.out.println("我是第二个服务提供者");
+		return userMapper.selectList(null);
+	}
+	
+	@Override
+	public void saveUser(User user) {
+		
+		userMapper.insert(user);
+	}
+	@Override
+	public void deleteUserById(Integer[] id) {
+		userMapper.deleteBatchIds(Arrays.asList(id));
+		
+	}
+}
